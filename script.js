3edf2d14a60f43470a44a16dee04cf2187c28660
David <davidjoshua285@gmail.com>
2023-01-27 02:41:44 +0800

    Add operator display and fix some bugs
    - Add portion that displays the chosen operator
    - Set 0 as the default value in the display
    - Fix the clear button, it now shows 0 in the display when clicked
    - Fix the backspace button, it now doesn't erase the 0 default value
    - Fix bug wherein one or multiple zeroes may appear before a non-zero
    digit

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
-const display = document.querySelector(".display");
+const operatorDisplay = document.querySelector(".operator-display");
+const numberDisplay = document.querySelector(".number-display");
 const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalButton = document.querySelector("#equal");
 const decimalButton = document.querySelector("#decimal");
 const clearButton = document.querySelector("#clear");
 const backspaceButton = document.querySelector("#backspace");
 
+let operand1, operand2, output;
+let clearOperand = false;
+let nextOperand = false;
+
 function displayDigit() {
-  display.textContent += this.id;
+  if (numberDisplay.textContent === "0") {
+    numberDisplay.textContent = "";
+    numberDisplay.textContent += this.id;
+  } else {
+    numberDisplay.textContent += this.id;
+  }
 }
 
 function addDecimal() {
-  if (display.textContent.includes(".") === false) {
-    display.textContent += ".";
+  if (numberDisplay.textContent.includes(".") === false) {
+    numberDisplay.textContent += ".";
   }
 }
 
 function clear() {
-  display.textContent = "";
+  numberDisplay.textContent = "0";
+  operatorDisplay.textContent = "";
+  output = operand1 = operand2 = 0;
 }
 
 function backspace() {
-  let displayContent = display.textContent;
-  display.textContent = displayContent.slice(0, -1);
+  if (numberDisplay.textContent !== "0") {
+    let numberDisplayContent = numberDisplay.textContent;
+    numberDisplay.textContent = numberDisplayContent.slice(0, -1);
+  }
 }
 
 numberButtons.forEach(button => {
