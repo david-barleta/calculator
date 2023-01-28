ed4e898992f0a634629c8af7ae023c18e9b8b203
David <davidjoshua285@gmail.com>
2023-01-29 03:26:11 +0800

    Fix some bugs
    - Fix bug wherein division by zero displays infinity, division by zero
    now displays undefined
    - Fix bug wherein operations can be done continuously if the equal
    button is clicked repeatedly even though there is no second operand yet
    or when an operation is finally done

--- script.js
+++ script.js
@@ -7,18 +7,30 @@ const decimalButton = document.querySelector("#decimal");
 const clearButton = document.querySelector("#clear");
 const backspaceButton = document.querySelector("#backspace");
 
-let operand1, operand2, operation, output;
-let nextOperand = false;
+let operand1, operand2, output;
+let operation = "none";
+let displayNextOperand = false; // If the display should clear the current input displayed to display the next operand
+let previousOperandCleared = false;
+let firstOperand = true; // If the input should be stored in the operand1 variable
+let secondOperand = false; // If the input should be stored in the operand2 variable
+let firstOperandStored = false;
+let secondOperandStored = false;
+
+
 
 function displayDigit() {
   if (numberDisplay.textContent === "0") {
     numberDisplay.textContent = "";
     numberDisplay.textContent += this.id;
   } else {
-    if (nextOperand) {
+    if (displayNextOperand) {
       numberDisplay.textContent = "";
       numberDisplay.textContent += this.id;
-      nextOperand = false;
+      displayNextOperand = false;
+      previousOperandCleared = true;
+      if (operatorDisplay.textContent === "=") {
+        operatorDisplay.textContent = "";
+      }
     } else {
       numberDisplay.textContent += this.id;
     }
@@ -36,7 +48,7 @@ function clear() {
   operatorDisplay.textContent = "";
   output = operand1 = operand2 = 0;
   operation = "none";
-  nextOperand = false;
+  displayNextOperand = false;
 }
 
 function backspace() {
@@ -51,49 +63,86 @@ function backspace() {
   }
 }
 
-function setOperation() {
-  operation = this.id;
-  operatorDisplay.textContent = this.textContent;
-  operand1 = +numberDisplay.textContent;
-  nextOperand = true;
-}
+// Operation functions
 
-function add(addend1, addend2) {
-  let sum = addend1 + addend2;
-  console.log("addition");
+function add() {
+  let sum = operand1 + operand2;
   return sum;
 }
 
-function subtract(minuend, subtrahend) {
-  let difference = minuend - subtrahend;
+function subtract() {
+  let difference = operand1 - operand2;
   return difference;
 }
 
-function multiply(multiplicand, multiplier) {
-  let product = multiplicand * multiplier;
+function multiply() {
+  let product = operand1 * operand2;
   return product;
 }
 
-function divide(dividend, divisor) {
-  let quotient = dividend / divisor;
+function divide() {
+  let quotient = operand1 / operand2;
+  if (quotient === Infinity) {
+    return "undefined";
+  }
   return quotient;
 }
 
 function operate() {
-  operand2 = +numberDisplay.textContent;
   if (operation === "addition") {
-    output = add(operand1, operand2);
+    output = add();
   } else if (operation === "subtraction") {
-    output = subtract(operand1, operand2);
+    output = subtract();
   } else if (operation === "multiplication") {
-    output = multiply(operand1, operand2);
+    output = multiply();
   } else if (operation === "division") {
-    output = divide(operand1, operand2);
+    output = divide();
   }
-  operatorDisplay.textContent = "=";
+}
+
+function storeFirstOperand() {
+  operand1 = +numberDisplay.textContent;
+  displayNextOperand = true;
+  firstOperand = false;
+  secondOperand = true;
+  firstOperandStored = true;
+}
+
+function storeSecondOperand() {
+  operand2 = +numberDisplay.textContent;
+  firstOperand = true;
+  secondOperand = false;
+  secondOperandStored = true;
+}
+
+function displayOutput() {
   numberDisplay.textContent = output;
-  output = operand1 = operand2 = 0;
-  operation = "none";  
+}
+
+function reset() {
+  output = operand1 = operand2 = undefined;
+  operation = "none";
+  firstOperandStored = secondOperandStored = false;
+  previousOperandCleared = false;
+}
+
+function equalButtonClicked() {
+  if (operation !== "none") {
+    if (previousOperandCleared === true) {
+      operatorDisplay.textContent = "=";
+      storeSecondOperand();
+      operate();
+      displayOutput();
+      reset();
+      displayNextOperand = true;
+    }
+  }
+}
+
+function operatorButtonClicked() {
+  operation = this.id;
+  operatorDisplay.textContent = this.textContent;
+  storeFirstOperand();
 }
 
 numberButtons.forEach(button => {
@@ -101,10 +150,10 @@ numberButtons.forEach(button => {
 })
 
 operatorButtons.forEach(button => {
-  button.addEventListener('click', setOperation);
+  button.addEventListener('click', operatorButtonClicked);
 })
 
-equalButton.addEventListener('click', operate);
+equalButton.addEventListener('click', equalButtonClicked);
 clearButton.addEventListener('click', clear);
 backspaceButton.addEventListener('click', backspace);
 decimalButton.addEventListener('click', addDecimal);
